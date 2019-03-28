e0afa55252412aa9c5813b9bb931991ec18245e9
Kennedy Mwangi <wanjaukm@gmail.com>
2019-03-28 15:50:55 +0300

    solved 'bad interpreter' bug

--- scripts/fastq-download.sh
+++ scripts/fastq-download.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/bin/bash
 #
 #Script to download fastq files from European Nucleotide Archive
 #Created March 28,2019 by Kennedy Mwangi
 #
 FILE=$1 #link to the file containing fastq url links to ENA FTP database
+OUTDIR=~/tbrucei_gcn/data/raw_data/
 
-cat ${FILE} | xargs -n1 wget$2
-echo "Done"
+cat ${FILE} | xargs -n1 wget$2 -P ${OUTDIR}
