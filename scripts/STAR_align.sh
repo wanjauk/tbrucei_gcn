61e85382ab8fa9bc713573151078cdbd7578387f
Kennedy Mwangi <wanjaukm@gmail.com>
2019-04-01 22:40:20 +0300

    add STAR alignment scripts, fastqc and HTSeq scripts

---
+++ scripts/STAR_align.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#Script to align reads on the reference genome
+#Created April 1, 2019 by Kennedy Mwangi
+#
+module load star/2.7.0e
+
+mkdir -p ~/tbrucei_gcn/results/STAR_align_output
+
+GENOME_INDEX=$1 #path to genome index directory.
+
+for fastq in $(ls ~/tbrucei_gcn/data/raw_data/*.fastq); do
+    basename=$(echo $fastq | cut -f1 -d '.')
+
+    STAR \
+        --runThreadN 4 \
+        --genomeDir $GENOME_INDEX \
+        --readFilesIn $fastq \
+        --outFileNamePrefix ../results/STAR_align_ouput/$basename
---
+++ scripts/STAR_genome_index.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#Script to create T. brucei genome index using STAR.
+#
+#Created on March 29, 2019 by Kennedy Mwangi
+#
+mkdir -p ~/tbrucei_gcn/data/STAR_genome
+
+module load star/2.7.0e
+
+INDEX_DIR=~/tbrucei_gcn/data/STAR_genome/
+GENOME_DIR=$1
+ANNOTATION_FILE=$2
+
+STAR --runThreadN 4 \
+--runMode genomeGenerate \
+--genomeDir $INDEX_DIR \
+--genomeFastaFiles $GENOME_DIR \
+--sjdbGTFfile $ANNOTATION_FILE \
+--sjdbOverhang 74 \
+--sjdbGTFtagExonParentTranscript Parent #GFF file being used instead of GTF
