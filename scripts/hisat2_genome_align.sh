3ee18a1ea30364a2c3ebcb22eeb42ce3c2436ce6
Kennedy Mwangi <wanjaukm@gmail.com>
2019-04-08 14:46:16 +0300

    solved bug in the script causing HISAT2 to exit with an exception

--- scripts/hisat2_genome_align.sh
+++ scripts/hisat2_genome_align.sh
@@ -5,12 +5,12 @@
 #
 module load hisat/2-2.1.0
 
-for fastq in $(ls ~/tbrucei_gcn/data/raw_data/*.fastq); do
-    basename=$(echo $fastq | cut -f1 -d '.')
+for fastq in  ~/tbrucei_gcn/data/raw_data/*.fastq; do
+    fqname=$(echo $fastq | cut -f1 -d '.')
         hisat2 \
             -x tbrucei_genome_index_hisat2 \
             -U ${fastq} \
-            -S ~/tbrucei_gcn/results/hisat2_align_results/${basename}.sam \
+            -S ${fqname}.sam \
             -p 8 \
-            --new-summary \
-
+            --new-summary ${fqname}.txt
+done
