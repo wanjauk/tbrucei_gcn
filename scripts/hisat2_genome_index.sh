4f10ac4bff408888032a17e0f26a51e856955114
Kennedy Mwangi <wanjaukm@gmail.com>
2019-04-08 10:30:23 +0300

    add hisat2 script to index genome

---
+++ scripts/hisat2_genome_index.sh
@@ -0,0 +1,11 @@
+#bin/bash
+#
+#Script to index T. brucei genome using HISAT2
+#Created April 8, 2019 by Kennedy Mwangi
+#
+module load hisat/2-2.1.0
+
+GENOME_FILE=$1
+
+
+hisat2-build ${GENOME_FILE} tbrucei_genome_index_hisat2
