15f32e15adf979238438552c7bbd69678808d902
Kennedy Mwangi <wanjaukm@gmail.com>
2019-04-10 15:52:03 +0300

    progress save; solved bugs in the script

--- scripts/counts-htseq.sh
+++ scripts/counts-htseq.sh
@@ -10,14 +10,14 @@ mkdir -p ~/tbrucei_gcn/results/HTSeq_count_results
 
 GFF_FILE=$1
 
-for sam_file in $(ls ~/tbrucei_gcn/data/raw_data/*.sam); do
+for sam_file in  ~/tbrucei_gcn/data/raw_data/*.sam; do
     sam_file_name=$(echo $sam_file | cut -f1 -d '.')
     
-        htseq-count \
-            --format=sam \
-            --stranded=yes \ #TODO: check whether stranded or not.
-            --type=exon \ #feature type
-            --idattr=Parent \
+        python /opt/apps/htseq/0.11.2/bin/htseq-count \
+            -f sam \
+            -s no \ #TODO: check whether stranded or not.
+            -t exon \ #feature type
+            -i Parent \
             $sam_file \
             $GFF_FILE > ${sam_file_name}.counts.txt
 done
--- scripts/hisat2_genome_align.sh
+++ scripts/hisat2_genome_align.sh
@@ -7,10 +7,11 @@ module load hisat/2-2.1.0
 
 for fastq in  ~/tbrucei_gcn/data/raw_data/*.fastq; do
     fqname=$(echo $fastq | cut -f1 -d '.')
-        hisat2 \
-            -x tbrucei_genome_index_hisat2 \
-            -U ${fastq} \
-            -S ${fqname}.sam \
-            -p 8 \
-            --new-summary ${fqname}.txt
+		hisat2 \
+		 -x tbrucei_genome_index_hisat2 \
+		 -U ${fastq} \
+		 -S ${fqname}.sam \
+		 -p 8 \
+		--summary-file ${fqname}.txt \
+		--new-summary
 done
