6f941ce14330904c7997823611c2aabe4c2d3ed1
Kennedy Mwangi <wanjaukm@gmail.com>
2019-05-21 17:27:29 +0300

    add code to analyze and export network

---
+++ scripts/network_export_graphml.R
@@ -0,0 +1,143 @@
+# This function was copied and used as is from https://github.com/iscb-dc-rsg/2016-summer-workshop/blob/master/3B-Hughitt-RNASeq-Coex-Network-Analysis/tutorial/README.md
+
+#' Converts an adjaceny matrix along with some optional vertex and edge
+#'  information to a GraphML graph and saves it to disk.
+#'
+#' @param adj_mat An n-by-n weighted or unweighted adjacency matrix normalized
+#' to contain values between 0 and 1.
+#' @param filename Name of file to save output to. If file already exists it
+#' will be overwritten. (default: network.graphml)
+#' @param weighted Whether or not the adjacency matrix should be treated as a 
+#' weighted graph. (default: TRUE)
+#' @param threshold For weighted networks, if a threshold value between 0 and 
+#' 1 is specified, all edges with weights below that value with be dropped from
+#'   the graph. (default: 0.5)
+#' @param max_edge_ratio The maximum number of edges per node in the network to
+#' allow. If the number of edges that would remain for the specified threshold
+#' exceeds this value, the threshold will be raised to reduce the number of
+#' edges remaining. (default: 3)
+#' @param nodeAttr A vector with length equal to the number of vertices in the 
+#' network, where the ith entry in the vector corresponds to some numeric or 
+#' string annotation that should be associated with the ith node in the 
+#' adjacency matrix. (default: NULL)
+#' @param nodeAttrDataFrame A data frame containing one or more columns 
+#' associated with the vertices in the graph.  The ith row of the dataframe 
+#' should correspond to the ith entry in the adjacency matrix. (default: NULL)
+#' @param edgeAttributes Extra attributes to associate with the graph edges,
+#' formatted as a list of matrices of the same dimension and names as the
+#' adjacency matrix.
+#'
+#' Examples
+#' --------
+#' export_network_to_graphml(adj_mat, filename='~/network.graphml',
+#'                           threshold=0.3, nodeAttrDataFrame=df)
+#'
+#' See Also
+#' --------
+#' 1. http://www.inside-r.org/packages/cran/WGCNA/docs/exportNetworkToCytoscape
+#' 2. http://graphml.graphdrawing.org/
+#'
+#' Returns
+#' -------
+#' An igraph graph object representing the exported graph.
+export_network_to_graphml <- function (adj_mat, filename=NULL, weighted=TRUE,
+                                       threshold=0.5, max_edge_ratio=3,
+                                       nodeAttr=NULL, nodeAttrDataFrame=NULL,
+                                       edgeAttributes=NULL, verbose=FALSE) {
+  library('igraph')
+  
+  # Determine filename to use
+  if (is.null(filename)) {
+    filename='network.graphml'
+  }
+  
+  # TODO 2015/04/09
+  # Add option to rescale correlations for each module before applying
+  # threshold (this is simpler than the previous approach of trying to
+  # determine a different threshold for each module)
+  #
+  # Still, modules with very low correlations should be given somewhat
+  # less priority than those with very high correlations.
+  
+  #module_colors <- unique(nodeAttrDataFrame$color)
+  #module_genes <- which(nodeAttrDataFrame$color == color)
+  #module_adjmat <- adj_mat[module_genes,]
+  #num_genes <- length(module_genes)
+  
+  # Adjust threshold if needed to limit remaining edges
+  max_edges <- max_edge_ratio * nrow(adj_mat)
+  
+  edge_to_total_ratio <- max_edges / length(adj_mat)
+  edge_limit_cutoff <- as.numeric(quantile(abs(adj_mat), 1 - edge_to_total_ratio))
+  
+  # Also choose a minimum threshold to make sure that at least some edges
+  # are left
+  min_threshold <- as.numeric(quantile(abs(adj_mat), 0.9999))
+  
+  threshold <- min(min_threshold, max(threshold, edge_limit_cutoff))
+  
+  # Remove edges with weights lower than the cutoff
+  adj_mat[abs(adj_mat) < threshold] <- 0
+  
+  # Drop any genes with no edges (TODO: Make optional)
+  orphaned <- (colSums(adj_mat) == 0)
+  adj_mat <- adj_mat[!orphaned, !orphaned]
+  
+  # Also remove annotation entries
+  if (!is.null(nodeAttr)) {
+    nodeAttr <- nodeAttr[!orphaned]
+  }
+  if (!is.null(nodeAttrDataFrame)) {
+    nodeAttrDataFrame <- nodeAttrDataFrame[!orphaned,]
+  }
+  
+  # Keep track of non-positive edges and rescale to range 0,1
+  is_zero     <- adj_mat == 0
+  is_negative <- adj_mat < 0
+  
+  adj_mat <- (abs(adj_mat) - threshold) / (max(adj_mat) - threshold)
+  adj_mat[is_zero] <- 0
+  adj_mat[is_negative] <- -adj_mat[is_negative]
+  
+  if (verbose) {
+    message(sprintf("Outputting matrix with %d nodes and %d edges", 
+                    nrow(adj_mat), sum(adj_mat > 0)))
+  }
+  
+  # Create a new graph and add vertices
+  # Weighted graph
+  if (weighted) {
+    g <- graph.adjacency(adj_mat, mode='undirected', weighted=TRUE, diag=FALSE)
+  } else {
+    adj_mat[adj_mat != 0] <- 1
+    g <- graph.adjacency(adj_mat, mode='undirected', diag=FALSE)
+  }
+  
+  # Add single node annotation from vector
+  if (!is.null(nodeAttr)) {
+    g <- set.vertex.attribute(g, "attr", value=nodeAttr)
+  }
+  
+  # Add node one or more node annotations from a data frame
+  if (!is.null(nodeAttrDataFrame)) {
+    for (colname in colnames(nodeAttrDataFrame)) {
+      g <- set.vertex.attribute(g, colname, value=nodeAttrDataFrame[,colname])
+    }
+  }
+  
+  edge_correlation_negative <- c()
+  
+  # neg_correlations[edge_list]
+  edge_list <- get.edgelist(g)
+  
+  for (i in 1:nrow(edge_list)) {
+    from <- edge_list[i, 1]    
+    to   <- edge_list[i, 2]    
+  }
+  
+  # Save graph to a file
+  write.graph(g, filename, format='graphml')
+  
+  # return igraph
+  return(g)
+}
