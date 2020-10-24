797d9ae77a38b96d4593d58c870e01bed3e49add
wanjauk <wanjaukm@gmail.com>
2020-10-24 11:43:08 +0300

    move tbrucei annotation packages to independent repos

--- tbrucei_annotation_packages/Trypanosoma.brucei.TREU927/R/zzz.R
+++
@@ -1,9 +0,0 @@
-## .onLoad gets the data.frame from the /data directory
-.onLoad <- function(libname, pkgname) {
-  load(system.file("data","graphInfo.rda",package=pkgname,
-                         lib.loc=libname))
-  OrganismDbi:::.loadOrganismDbiPkg(pkgname=pkgname,
-                          graphInfo=graphInfo)
-}
-
-
