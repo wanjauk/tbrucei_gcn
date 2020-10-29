9e690de8202aa9f66d91b2366c8ae748e3f98311
wanjauk <wanjaukm@gmail.com>
2020-10-29 15:48:07 +0300

    modify path variable in fastqc script

--- scripts/analysis/generate-fastqc-reports.sh
+++ scripts/analysis/generate-fastqc-reports.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #
 #Script to run FastQC reports using the FastQC tool
+# ./generate-fastqc-reports.sh /data/kwanjau/savage/*.fastq ../../results/figures/fastqc_reports/savage/; \ 
+# ./generate-fastqc-reports.sh /data/kwanjau/telleria/*.fastq ../../results/figures/fastqc_reports/telleria/
 #
 #load fastqc module
-module load fastqc/0.11.4
+# module load fastqc/0.11.4
 
-FASTQ_DIR=../data/raw_data/*.fastq
+# fastq files directory
+FASTQ_DIR=$1
 
-#create output directory if it doesn't exist.
-mkdir -p ../results/fastqc_reports
-
-REPORTS_DIR=../results/fastqc_reports/
+# fastqc reports directory
+REPORTS_DIR=$2
 
 for file in ${FASTQ_DIR}; do
    fastqc -f fastq -o ${REPORTS_DIR} ${file}
