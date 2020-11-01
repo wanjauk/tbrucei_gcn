82618cd3b85e541be355f36e69f6d4cfac57bb55
wanjauk <wanjaukm@gmail.com>
2020-11-01 10:09:01 +0300

    re-organize fastqc reports directories

--- scripts/analysis/generate-fastqc-reports.sh
+++ scripts/analysis/generate-fastqc-reports.sh
@@ -1,18 +1,24 @@
-#!/bin/bash
+# #!/bin/bash
 #
 #Script to run FastQC reports using the FastQC tool
-# ./generate-fastqc-reports.sh /data/kwanjau/savage/*.fastq ../../results/figures/fastqc_reports/savage/; \ 
-# ./generate-fastqc-reports.sh /data/kwanjau/telleria/*.fastq ../../results/figures/fastqc_reports/telleria/
+#
+# USAGE:
+# bash generate-fastqc-reports.sh ../../results/figures/fastqc_reports/savage ../../data/raw/savage 
+# bash generate-fastqc-reports.sh ../../results/figures/fastqc_reports/telleria ../../data/raw/telleria
+#
+# make directory to store the results
+mkdir -p ../../results/figures/fastqc_reports/savage/; 
+mkdir -p ../../results/figures/fastqc_reports/telleria/
 #
 #load fastqc module
 # module load fastqc/0.11.4
 
-# fastq files directory
-FASTQ_DIR=$1
-
 # fastqc reports directory
-REPORTS_DIR=$2
+REPORT_DIR=$1
 
-for file in ${FASTQ_DIR}; do
-   fastqc -f fastq -o ${REPORTS_DIR} ${file}
+# fastq files directory
+FASTQ_DIR=$2
+
+for file in $FASTQ_DIR/*.fastq; do
+   fastqc ${file} -o ${REPORT_DIR} -f fastq
 done
