5c939c300f3b55d65a87083cfa5b439eea766b41
wanjauk <wanjaukm@gmail.com>
2020-11-14 17:42:54 +0300

    add code for telleria dupRadar analysis

--- scripts/analysis/run-dupRadar.R
+++ scripts/analysis/run-dupRadar.R
@@ -6,7 +6,7 @@ source(here::here("scripts","analysis","settings.R"))
 gtf_file <- here::here("data","scratch","concatenated_genomes",
                        "brucei-morsitans_annotations.gtf")
 stranded <- 0
-paired <- FALSE
+paired <- FALSE # for savage's data
 threads <- 10
 
 # path to bam files data
@@ -14,33 +14,56 @@ bam_file <- list.files(here::here("data","scratch","mark-duplicates-output","sav
                        pattern = ".bam",
                        full.names = TRUE)
 
-for (file in bam_file){
-      
-  #get the file name
-  file_name <- gsub(pattern = "\\.dupMarked.bam$", "", basename(file))
+# for (file in bam_file){
+#       
+#   #get the file name
+#   file_name <- gsub(pattern = "\\.dupMarked.bam$", "", basename(file))
+# 
+#   # Duplication rate analysis
+#   dm <- analyzeDuprates(file, gtf_file, stranded, paired, threads)
+#   
+#   #Plots
+#   png(filename = here::here("results","figures","duplication_rate",paste0(file_name,".png")))
+#   duprateExpDensPlot(DupMat=dm)
+#   title(paste0(file_name))
+#   dev.off()
+#   
+#   # # Boxplot
+#   # duprateExpBoxplot(DupMat=dm)
+#   
+# }
 
-  # Duplication rate analysis
-  dm <- analyzeDuprates(file, gtf_file, stranded, paired, threads)
-  
-  #Plots
-  png(filename = here::here("results","figures","duplication_rate",paste0(file_name,".png")))
-  duprateExpDensPlot(DupMat=dm)
-  title(paste0(file_name))
-  dev.off()
-  
-  # # Boxplot
-  # duprateExpBoxplot(DupMat=dm)
-  
-}
+#########################################
+# Telleria paired-end data processing
+#########################################
 
-# Exclusion of the following samples was done after analysis showed they
-# failed quality control.
+#Parameters
+telleria_paired <- TRUE
 
-# remove 3 samples that have technical duplicates (SRR039951, SRR039937, SRR039938)
-samples.metadata <- samples.metadata[-15,]
-samples.metadata <- samples.metadata[-9,]
-samples.metadata <- samples.metadata[-8,]
+# path to bam files data
+telleria_bam_file <- list.files(here::here("data","scratch","mark-duplicates-output","telleria"),
+                       pattern = ".bam",
+                       full.names = TRUE)
+#get the file name
+telleria_file_name <- gsub(pattern = "\\.dupMarked.bam$", "", basename(telleria_bam_file))
 
-# save the samples metadata
-saveRDS(samples.metadata, here::here("data","raw","samples.metadata.clean.RDS"))
+# Duplication rate analysis
+dm <- analyzeDuprates(telleria_bam_file, gtf_file, stranded, telleria_paired, threads)
 
+#Plots
+png(filename = here::here("results","figures","duplication_rate",paste0(telleria_file_name,".eps")))
+duprateExpDensPlot(DupMat=dm)
+title(paste0(telleria_file_name))
+dev.off()
+
+# # Exclusion of the following samples was done after analysis showed they
+# # failed quality control.
+# 
+# # remove 3 samples that have technical duplicates (SRR039951, SRR039937, SRR039938)
+# samples.metadata <- samples.metadata[-15,]
+# samples.metadata <- samples.metadata[-9,]
+# samples.metadata <- samples.metadata[-8,]
+# 
+# # save the samples metadata
+# saveRDS(samples.metadata, here::here("data","raw","samples.metadata.clean.RDS"))
+# 
